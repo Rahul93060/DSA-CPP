630e172b6901a8ec2bb4d408f65f67742966c25d
RAHUL <itzzrahul93060@gmail.com>
2026-07-02 14:44:57 +0530

    Time: 12 ms (96.33%), Space: 24.1 MB (39.4%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
@@ -2,7 +2,29 @@ class Solution {
 public:
     int maxProductDifference(vector<int>& nums) {
         int n=nums.size();
-        sort(nums.begin(),nums.end());
-        return (nums[n-1]* nums[n-2]) - (nums[0] * nums[1]);
+        // sort(nums.begin(),nums.end());
+        // return (nums[n-1]* nums[n-2]) - (nums[0] * nums[1]);
+        int a=INT_MIN;
+        int b=INT_MIN;
+        int c=INT_MAX;
+        int d=INT_MAX;
+        for(int x:nums){
+            if(x>=a){
+                b=a;
+                a=x;
+            }else if(x>b){
+                b=x;
+            }
+
+
+            if(x<=c){
+                d=c;
+                c=x;
+            }else if(x<d){
+                d=x;
+            }
+        }
+        // return d;
+        return (a*b) - (c*d);
     }
 };
