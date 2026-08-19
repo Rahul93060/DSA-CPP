fa13813b7610631bf9d529ef476d3eaaa08457b1
RAHUL <itzzrahul93060@gmail.com>
2026-08-20 01:49:07 +0530

    Time: 0 ms (100%), Space: 9.4 MB (10.48%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,6 +1,49 @@
 class Solution {
 public:
+    void find_lps(vector<int>& lps,string needle){
+
+        int pre=0;
+        int suf=1;
+        while(suf<needle.size()){
+
+            if(needle[pre]== needle[suf]){
+                lps[suf]=pre+1;
+                pre++;
+                suf++;
+            }else{
+                if(pre==0){
+                    lps[suf] =0;
+                    suf++;
+                }else{
+                    pre=lps[pre-1];
+                }
+            }
+        }
+    }
     int strStr(string haystack, string needle) {
-        return haystack.find(needle);
+        vector<int>lps(needle.size(),0);
+        find_lps(lps,needle);
+
+        int first=0;
+        int sec=0;
+
+        while(first<haystack.size()  && sec<needle.size() ){
+
+            if(haystack[first]==needle[sec]){
+                first++;
+                sec++;
+            }else{
+                if(sec==0){
+                    first++;
+                }else{
+                    sec=lps[sec-1];
+                }
+            }
+        }
+        if(sec == needle.size()){
+            return first-sec;
+        }else{
+            return -1;
+        }
     }
 };
