ac9fb314226e345aed919e35fdedd579ade89143
RAHUL <itzzrahul93060@gmail.com>
2026-09-07 10:48:15 +0530

    Time: 0 ms (100%), Space: 11.1 MB (89.45%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int>ans;
+        int count=0;
+        for(int i=0;i<heights.size();i++){
+            ans.push_back(heights[i]);
+        }
+        sort(ans.begin(),ans.end());
+        for(int i=0;i<heights.size();i++){
+            if(ans[i]!=heights[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+};
