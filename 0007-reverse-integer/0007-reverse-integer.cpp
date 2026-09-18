18dff4bf40b48dc1b64f7497dd53669ec2a4023c
RAHUL <itzzrahul93060@gmail.com>
2026-09-18 20:30:46 +0530

    Time: 0 ms (100%), Space: 8.4 MB (97.99%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long rev = 0; // use long long to detect overflow
+        while (x != 0) {
+            int digit = x % 10;
+            rev = rev * 10 + digit;
+            x /= 10;
+            if (rev > INT_MAX || rev < INT_MIN)  // check overflow
+                return 0;
+        }
+        return (int)rev;
+    }
+};
