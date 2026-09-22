a94a753344a29a5e70100ce5c7adbf5bc7e40703
RAHUL <itzzrahul93060@gmail.com>
2026-09-22 20:40:38 +0530

    Time: 7 ms (63.07%), Space: 43.6 MB (98.12%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,10 +1,56 @@
 class Solution {
     public String reverseWords(String s) {
-        // approch 1 : trim , split on " " ten reverse then use the join function 
-        String strimed= s.trim();
-        String[] str=strimed.split("\\s+");
-        List<String> liststr = Arrays.asList(str);
-        Collections.reverse(liststr);
-        return String.join(" ",liststr);
+        int left=0;
+        int right=s.length()-1;
+        while(left<s.length() && s.charAt(left) == ' '){
+            left++;
+        }
+        while(right >0 && s.charAt(right) == ' '){
+            right--;
+        }
+        StringBuilder str = new StringBuilder();
+
+        while(left<=right){
+            if(s.charAt(left) != ' '){
+                str.append(s.charAt(left));
+            }else if(str.charAt(str.length()-1)!= ' '){
+                str.append(s.charAt(left));
+            }
+            left++;
+        }
+
+        int i=0,
+         j=str.length()-1;
+
+            while(i<j){
+                char temp = str.charAt(i);
+                str.setCharAt(i,str.charAt(j));
+                str.setCharAt(j,temp);
+                i++;
+                j--;
+            }
+
+            int start=0;
+            int end=0;
+            while(start<str.length()){
+
+                while(end<str.length() && str.charAt(end) != ' '){
+                    end++;
+                }
+                i=start;
+                j=end-1;
+                while(i<j){
+                char temp = str.charAt(i);
+                str.setCharAt(i,str.charAt(j));
+                str.setCharAt(j,temp);
+                i++;
+                j--;
+            }
+            start=end+1;
+            end=end+1;
+
+            }
+
+        return str.toString();
     }
 }
